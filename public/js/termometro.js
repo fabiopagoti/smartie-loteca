2093ddd23cfaff13b052ca19d3f0c77ac29a0046
Fabio Pagoti <fabio@duxinnovation.com>
2017-09-09 19:07:58 -0300

    Tentativas frustradas

--- public/js/termometro.js
+++ public/js/termometro.js
@@ -16,13 +16,34 @@ function verificaAposta() {
 			// dataType: 'json',
 			data: JSON.stringify(aNumerosSelecionados),
 		})
-		.done(function(a,b,c) {
+		.done(function(a, b, c) {
 			window.location = a;
 		})
 		.fail(function() {
 			console.log('error');
 		})
-		.always(function(a,b,c) {
+		.always(function(a, b, c) {
 
 		});
+}
+
+
+function onClickNumero(oNumero) {
+	// console.log(oNumero.classList)
+return
+	console.log('inicio')
+	setTimeout(function() {
+		// oNumero.classList.remove('focus')
+		// oNumero.classList.remove('active')
+		// document.querySelector('#contador').tabIndex = -1
+		// document.querySelector('#contador').focus()
+		// document.querySelector('#contador').select()
+		// document.querySelector('#contador').click()
+		//   	$(document.querySelector('#contador')).trigger('click');
+		//   	$(document.querySelector('#contador')).trigger('touchstart');
+
+		console.log('fim')
+	}, 100);
+	console.log('depois')
+
 }
