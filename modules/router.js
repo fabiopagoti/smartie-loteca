45c68d79b2a073eeda4c518da31fd47a4f8a4396
Fabio Pagoti <fabio@duxinnovation.com>
2017-09-09 01:58:05 -0300

    barra superior com links ativos, logo volta na home

--- modules/router.js
+++ modules/router.js
@@ -1,6 +1,6 @@
 /*************************
-*	Rotas da Aplicacao
-**************************/
+ *	Rotas da Aplicacao
+ **************************/
 
 const lotofacil = require('./data/lotofacil.json')
 const megasena = require('./data/megasena.json')
@@ -11,15 +11,20 @@ const assert = require('assert')
 const defineRouter = (app) => {
 
 	app.get('/', (request, response) => {
-		response.render('home')
+		response.render('home', {
+			home: true
+		})
 	})
 
 	app.get('/sobre', (request, response) => {
-		response.render('sobre')
+		response.render('sobre', {
+			sobre: true
+		})
 	})
 
 	app.get('/termometro', (request, response) => {
 		response.render('termometro', {
+			termometro: true,
 			numeros: {
 				linha1: {
 					no: [...Array(5).keys()].map(x => ++x)
@@ -41,47 +46,62 @@ const defineRouter = (app) => {
 	})
 
 	app.post('/termometro', (request, response) => {
-		try{
+		try {
 			let oResult = controller.parImpar(request.body);
 			let sUrl = url.format({
 				pathname: '/resultado',
 				query: oResult
 			})
 			response.send(sUrl)
-		}catch(e){
+		} catch (e) {
 			response
-			.status(400)
-			.send(e.message)
+				.status(400)
+				.send(e.message)
 		}
 	})
-	
+
 	app.get('/resultado', (request, response) => {
 		let oQuery = request.query
-		assert.ok(oQuery)
-
-		response.render('resultado', oQuery)
+		try {
+			assert.ok(oQuery.frequencia)
+			response.render('resultado', oQuery)
+		} catch (e) {
+			response.redirect('termometro', {
+				termometro: true
+			})
+		}
 	})
 
 
 	app.get('/estatisticas/:loteria?', (request, response) => {
 		let sLoteria = request.params.loteria
-		switch (sLoteria){
+		switch (sLoteria) {
 			case "lotofacil":
-			response.render('loteria', lotofacil)
-			break
+				response.render('loteria', {
+					estatisticas: true,
+					loteria: lotofacil
+				})
+				break
 
 			case "megasena":
-			response.render('loteria', megasena)
-			break
+				response.render('loteria', {
+					estatisticas: true,
+					loteria: megasena
+				})
+				break
 
 			default:
-			response.render('estatisticas')
-			break
+				response.render('estatisticas', {
+					estatisticas: true
+				})
+				break
 		}
 	})
 
 	app.get('/quem-somos', (request, response) => {
-		response.render('quem-somos')
+		response.render('quem-somos', {
+			quemSomos: true
+		})
 	})
 
 	return app
